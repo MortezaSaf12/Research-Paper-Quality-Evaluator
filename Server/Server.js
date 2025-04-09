fcab4158f21769ed340aa0c005e7085add3e96a2
MortezaSaf12 <morsaf731@gmail.com>
2025-04-09 08:53:07 +0200

    Integrated Gemini into project

--- Server/Server.js
+++ Server/Server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'Client')));
 app.use('/api/pdf', pdfRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   
